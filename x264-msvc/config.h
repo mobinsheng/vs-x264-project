7867421c9ecc7820621c4bcbc28ba18028e41310
Unknown <574923692@qq.com>
2018-01-16 17:26:31 +0800

    提交代码

---
+++ x264-msvc/config.h
@@ -0,0 +1,31 @@
+#define ARCH_X86 1 ///< x86架构
+#define SYS_WINDOWS 1 ///< windows系统
+#define STACK_ALIGNMENT 16 ///< 按照16字节对齐
+#define HAVE_WIN32THREAD 1 ///< 使用win32的线程
+#define HAVE_THREAD 1 ///< 有线程系统
+#define HAVE_LOG2F 1 ///< log函数
+#define HAVE_VECTOREXT 1 ///< 向量扩展
+// #define fseek fseeko64
+// #define ftell ftello64
+#define HAVE_GPL 1 ///< gpl
+#define HAVE_MALLOC_H 0
+#define HAVE_ALTIVEC 0
+#define HAVE_ALTIVEC_H 0
+#define HAVE_MMX 0
+#define HAVE_ARMV6 0
+#define HAVE_ARMV6T2 0
+#define HAVE_NEON 0
+#define HAVE_BEOSTHREAD 0
+#define HAVE_POSIXTHREAD 0
+#define HAVE_SWSCALE 0
+#define HAVE_LAVF 0
+#define HAVE_FFMS 0
+#define HAVE_GPAC 0
+#define HAVE_AVS 0
+#define HAVE_INTERLACED 0
+#define HAVE_CPU_COUNT 0
+#define HAVE_OPENCL 0
+#define HAVE_THP 0
+#define HAVE_LSMASH 0
+#define HAVE_X86_INLINE_ASM 0
+#define HAVE_AS_FUNC 0
